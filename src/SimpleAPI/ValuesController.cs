37a311c9bba183391b23ba1e9e13595f12b7ce0a
sandeepkaran1 <sandeepkaran1@outlook.com>
2024-07-12 15:34:24 +0530

    first commit

---
+++ src/SimpleAPI/ValuesController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI
+{
+    [ApiController]
+    [Route("api/[controller]")]
+     public class ValuesController :Controller
+    {
+        // GET: api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/values/5
+        public string Get(int id)
+        {
+            return "sandeep";
+        }
+
+        // POST: api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
