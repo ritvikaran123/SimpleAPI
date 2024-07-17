aa6f470a7b6824613eebf32fa44747bd012fe23b
sandeepkaran1 <sandeepkaran1@outlook.com>
2024-07-17 14:49:35 +0530

    first triggerh

--- src/SimpleAPI/Program.cs
+++ src/SimpleAPI/Program.cs
@@ -14,11 +14,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezinhhhhh", "Bracinghhh", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
--- test/SimpleAPI.Tests/obj/Debug/net8.0/SimpleAPI.Tests.AssemblyInfo.cs
+++ test/SimpleAPI.Tests/obj/Debug/net8.0/SimpleAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a311c9bba183391b23ba1e9e13595f12b7ce0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f447acdb3f96d8466be3c31e2fb97ff7cfedcd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
