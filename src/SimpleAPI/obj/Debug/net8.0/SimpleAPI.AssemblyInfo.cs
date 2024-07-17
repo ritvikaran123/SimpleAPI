1a4d7b9ba22e2aa8d13ff156e49a5a686e89f772
sandeepkaran1 <sandeepkaran1@outlook.com>
2024-07-17 14:52:13 +0530

    first triggerh

--- src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
+++ src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f447acdb3f96d8466be3c31e2fb97ff7cfedcd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861633cfaa197ccd45e7461d9217fa8dfbc27bc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
