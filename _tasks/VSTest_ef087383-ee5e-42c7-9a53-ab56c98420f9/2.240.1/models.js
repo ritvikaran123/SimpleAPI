52b420e749bb61113c467ac70d0396dc77eedd30
ritvikaran123 <ritvikaran123@gmail.com>
2024-07-18 22:18:41 +0530

    Delete _tasks directory

--- _tasks/VSTest_ef087383-ee5e-42c7-9a53-ab56c98420f9/2.240.1/models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BatchingType = void 0;
-var BatchingType;
-(function (BatchingType) {
-    BatchingType[BatchingType["TestCaseBased"] = 0] = "TestCaseBased";
-    BatchingType[BatchingType["TestExecutionTimeBased"] = 1] = "TestExecutionTimeBased";
-    BatchingType[BatchingType["AssemblyBased"] = 2] = "AssemblyBased";
-})(BatchingType || (exports.BatchingType = BatchingType = {}));
