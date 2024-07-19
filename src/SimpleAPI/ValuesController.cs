d930d2b34051e736bd53e77e5d49cdbfe154c3a1
sandeepkaran1 <sandeepkaran1@outlook.com>
2024-07-19 15:32:10 +0530

    first commjjit

--- src/SimpleAPI/ValuesController.cs
+++ src/SimpleAPI/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI
         // GET: api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1f", "valuekj2" };
+            return new string[] { "value1f", "valuekj2o" };
         }
        [HttpGet("lis")]
         // GET: api/values/5
--- src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
+++ src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50928e20a0013ac46e8f34a64e0a87f53fd2ec9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57f0ee249220d4a6094c25866d092f7465a4c43e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
